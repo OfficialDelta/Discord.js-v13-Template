b471d125b14e6e9ee1e85968cc62e15cee2b39a4
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2021-08-09 13:51:30 -0400

    fixed the test returning [object Object]

--- subcommands/subcommandtest/test.js
+++ subcommands/subcommandtest/test.js
@@ -1,6 +1,6 @@
 module.exports = {
     async execute(interaction) {
-        const input = interaction.options.get('input') ?? false
+        const input = interaction.options.get('input')?.value ?? false
 
         const response = input ? `You inputted: ${input}` : "You didn't input anything"
 
